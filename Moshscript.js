d6783f655509f31faca57ff63d37678b81056ca9
Luke Alvarado <lukealvaradoteacher@gmail.com>
2023-07-28 01:22:17 -0700

    Initial Commit

---
+++ Moshscript.js
@@ -0,0 +1,13 @@
+
+console.log('Hello People')
+let name = 'Mosh'
+console.log(name);
+
+let firstName = 'Mosh';
+let lastName = 'Hamedani';
+console.log(lastName);
+
+let interestRate = 0.3;
+interestRate = 1;
+
+
